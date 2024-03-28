5922d1df1079dcb162e795c3160e3e7f13824fc2
sanmatias <metalmatia@gmail.com>
2024-03-28 19:19:54 -0300

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,3 @@ $(document).ready(function(){
     $('[data-bs-toggle="tooltip"]').tooltip();
 
 });
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
-
-alert("prueba");
